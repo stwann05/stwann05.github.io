d3b3ea0df3f9c7dca5a2c102563a107ba46b63ec
awann-4 <forwork4805@gmail.com>
2025-02-10 23:53:15 +0700

    menambahkan event click di luar bar, dan menambahkan animation mengunakan AOS terahir di about section

--- js/script.js
+++ js/script.js
@@ -5,10 +5,23 @@ document.addEventListener("DOMContentLoaded", function () {
     whatsappButton.addEventListener("click", function () {
       const phoneNumber = "6285962687860"; // Ganti dengan nomor WhatsApp kamu tanpa +
       const message = encodeURIComponent(
-        "Halo! Saya tertarik dengan layanan Anda."
+        "Halo! Saya ingin bekerja sama dengan anda."
       );
       const whatsappUrl = `https://wa.me/${phoneNumber}?text=${message}`;
       window.open(whatsappUrl, "_blank");
     });
+  }
+});
+
+// Event saat ketuk di luar taskbar
+
+document.addEventListener("click", function (event) {
+  const menu = document.getElementById("navbarNav");
+  const button = document.getElementById("Hamburger-button");
+
+  if (!menu || !button) return;
+
+  if (!menu.contains(event.target) && !button.contains(event.target)) {
+    menu.classList.remove("show");
   }
 });
